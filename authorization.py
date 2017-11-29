ce9d25711f9f6e45cc04bd0e66a1eb582afbde31
John Kastner <john.h.kastner@gmail.com>
2017-11-29 10:51:31 -0500

    fixed listener example

--- authorization.py
+++ authorization.py
@@ -1,20 +1,24 @@
 import tweepy
-from tweepy import OAuthHandler
 
-consumer_key = 'qXFcavM2ZXwduSzUn5dlpE2XH'
-consumer_secret = 'fhABJUgf0dJ5bCwvLtrnry5bKggRAEgqf71OQgLgQYq4ueDKOE'
-access_token = '935886161217839104-p3rzIm3HfvhaE0THUQSf7hAfDVjIvJ2'
-access_secret = '8E9tKooQeydsZwhuuFWuuoHJlS57oOyGShuhqy5323TgC'
-
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
-
-api = tweepy.API(auth)
-
-class GeoStreamListener(StreamListener):
+class GeoStreamListener(tweepy.StreamListener):
     hashtags = {'red': ["#Hillaryforprison", "#PresidentTrump", "#MakeAmericaGreatAgain", "#MAGA"], 'blue': ["#LoveTrumpsHate", "#NotMyPresident", "#PresidentClinton"]}
     def on_status(self, status):
         print(status.text)
     
     def collect_tweets(self):
         return None #Will be revised later
+
+consumer_key = 'qXFcavM2ZXwduSzUn5dlpE2XH'
+consumer_secret = 'fhABJUgf0dJ5bCwvLtrnry5bKggRAEgqf71OQgLgQYq4ueDKOE'
+access_token = '935886161217839104-p3rzIm3HfvhaE0THUQSf7hAfDVjIvJ2'
+access_secret = '8E9tKooQeydsZwhuuFWuuoHJlS57oOyGShuhqy5323TgC'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+myListener = GeoStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener = myListener)
+
+myStream.filter(track=['MAGA'])
