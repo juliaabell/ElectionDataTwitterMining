ee72f66106f0bf90e011a2fb45fda2d5af9c042d
jcelt64 <31477426+jcelt64@users.noreply.github.com>
2017-12-06 11:21:29 -0500

    Update geoccode.py

--- geoccode.py
+++ geoccode.py
@@ -1,13 +1,16 @@
 import geograpy
+from geopy.geocoders import Bing
+import json
 
-def extract_location(text):
-    e = geograpy.Extractor(text)
+def geocode_tweets(tweet, fil):
+    geograpy.places.PlaceContext(['United States'])
+    pc.set_countries()
+    e = geograpy.Extractor(text=tweet)
     e.find_entities()
-    return e.places
-
-def geocode_tweets(text):
-    e = geograpy.Extractor(text)
-    e.find_entities()
-    e.places[0]
-    
-    
+    place_name = e.places[0]
+    geolocator = Bing()
+    location = geolocator.geocode(place_name)
+    with open(fil, 'r+') as tweet_file:
+        data = json.load(tweet_file)
+        data['coordinates']['coordinates'] = location.longitude, location.latitude
+        
