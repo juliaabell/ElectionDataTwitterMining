391a35023dfde321725e7a820b42ff5b1e2bf10c
John Kastner <john.h.kastner@gmail.com>
2017-12-10 16:06:53 -0500

    Updated Geocoding code

--- code/twitter-data.py
+++ code/twitter-data.py
@@ -21,18 +21,8 @@ class Tweet():
         return self._text
 
     @property
-    def x(self):
-        if self._coords is not None:
-            return self._coords[0]
-        else:
-            return None
-
-    @property
-    def y(self):
-        if self._coords is not None:
-            return self._coords[0]
-        else:
-            return None
+    def coords(self):
+      return self._coords
 
 class GeoStreamListener(tweepy.StreamListener):
     def __init__(self, tags, count):
@@ -42,37 +32,18 @@ class GeoStreamListener(tweepy.StreamListener):
         self._stream = None
         self._max_count = count
 
-    def _geocode_tweet(self, tweet):
-
-        # If the tweet contains geographic data just return it
-        if tweet.place is not None:
-            return tweet.place.bounding_box.corner()
-
-        # Otherwise use geograpy + geopy to guess at location
-        geograpy.places.PlaceContext(['United States'])
-        e = geograpy.Extractor(text=tweet.text.encode('ascii', 'replace'))
-        e.find_entities()
-    
-        geolocator = Nominatim()
-        for place_name in e.places:
-            try:
-                location = geolocator.geocode(place_name)
-                if location is not None:
-                    return (location.latitude, location.longitude)
-            except: 
-                pass
-
-        # give up
-        return None
-
     def on_status(self, status):
         if len(self._tweet_collection) < self._max_count:
-            location = self._geocode_tweet(status)
-            if location is not None:
-                tweet = Tweet(status.text, location)
-                self._tweet_collection.append(tweet)
-                if len(self._tweet_collection) >= self._max_count:
-                    self.disconnect_stream()
+            if status.place is not None:
+                print 'found geotagged tweet'
+                tweet = Tweet(status.text, status.place.bounding_box.corner)
+            else:
+                tweet = Tweet(status.text, None)
+
+            self._tweet_collection.append(tweet)
+            if len(self._tweet_collection) >= self._max_count:
+                self.disconnect_stream()
+
         else:
             self.disconnect_stream()
 
@@ -98,8 +69,9 @@ class GeoStreamListener(tweepy.StreamListener):
 class GeoStreamPartioner():
 
     # partions :: Dict String [String]
-    def __init__(self, partions, count):
+    def __init__(self, partions, count, geolocator):
         self._listeners = {k : GeoStreamListener(v, count) for k, v in partions.iteritems()}
+        self._geolocator = geolocator
 
     def start_streams(self, auth):
         for k in self._listeners:
@@ -113,21 +85,47 @@ class GeoStreamPartioner():
         return {k : v.collect_tweets() for k, v in self._listeners.iteritems()}
 
     def csv_dump(self, filename):
+        print 'started geotaggin'
         with open(filename, 'wb') as tweet_file:
             writer = csv.writer(tweet_file)#, quoting=csv.QUOTE_NONNUMERIC)
             regex = re.compile(r'\s+')
             for name, tweets in self.collect_partions().iteritems():
                 for tweet in tweets:
                     text = regex.sub(' ', tweet.text.encode('ascii', 'replace'))
-                    writer.writerow([name, text, tweet.x, tweet.y])
 
-    def stream_open(self):
+                    if tweet.coords is None:
+                        coords = self._geocode_tweet(text)
+                        print 'coded', coords
+                    else:
+                        coords = tweet.coords
+                        print 'place', coords
+
+                    if coords is not None:
+                        writer.writerow([name, text, coords[0], coords[1]])
+
+    def stream_open(self):  
         r = False
         for k in self._listeners:
             c = self._listeners[k].connected()
             r = r or c
         return r
 
+    def _geocode_tweet(self, text):
+        # Try to use geograpy + geopy to guess at location
+        geograpy.places.PlaceContext(['United States'])
+        e = geograpy.Extractor(text=text)
+        e.find_entities()
+
+        for place_name in e.places:
+            try:
+                location = self._geolocator.geocode(place_name)
+                if location is not None:
+                    return (location.latitude, location.longitude)
+            except: 
+                pass
+
+        # give up
+        return None
 
 consumer_key = 'qXFcavM2ZXwduSzUn5dlpE2XH'
 consumer_secret = 'fhABJUgf0dJ5bCwvLtrnry5bKggRAEgqf71OQgLgQYq4ueDKOE'
@@ -139,7 +137,8 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
  
-partioner = GeoStreamPartioner({'Blue': ['#ManchesterDerby'], 'Red': ['#humanrightsday']}, 10)
+partioner = GeoStreamPartioner({'Blue': ['#ManchesterDerby'], 'Red': ['#humanrightsday']}, 10, Nominatim())
+
 partioner.start_streams(api.auth)
 
 while(partioner.stream_open()):
