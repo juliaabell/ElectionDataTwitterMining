9c24fd5ef449baa19817323f49d69ba254969faf
jcelt64 <31477426+jcelt64@users.noreply.github.com>
2017-12-11 10:12:10 -0500

    Update geovisualize.py

--- geovisualize.py
+++ geovisualize.py
@@ -13,26 +13,27 @@ import fiona.crs
 import csv
 
 #Makes two fiona shapefiles from the csv dataset, separating them by party
-def convert_csv(data_csv, redfile, bluefile):
-    newschema = {'geometry': 'Point', 'properties': {'text':'str', 'party':'str'}
-    with fiona.open(redfile, 'w', crs=from_epsg(4296) driver = "ESRI Shapefile", schema= newschema) as red:
-        with fiona.open(bluefile, 'w', crs=from_epsg(4296), driver = "ESRI Shapefile", schema=newschema) as blue:
+def convert_csv(data_csv, red_file, blue_file):
+    newschema = {'geometry': 'Point', 'properties': {'text': 'str', 'party':'str'}}
+    with fiona.open(red_file, 'w', driver="ESRI Shapefile", schema = newschema, crs = from_epsg(4296)) as red:
+        with fiona.open(blue_file, 'w', driver="ESRI Shapefile", schema = newschema, crs = from_epsg(4296)) as blue:
             with open(data_csv, 'rb') as f:
-            reader = csv.DictReader(f)
+                reader = csv.DictReader(f)
                 for row in reader:
                     if row[0] == 'Red':
                         point = Point(float(row[2]), float(row[3]))
-                        output.write({'properties': {'text': row[1], 'party': row[0]},'geometry': mapping(point)})
+                        red.write({'properties': {'text': row[1], 'party': row[0]},'geometry': mapping(point)})
                     elif row[0] == 'Blue':
                         point = Point(float(row[2]), float(row[3]))
-                        output.write({'properties': {'text': row[1]'party': row[0},'geometry': mapping(point)})
+                        blue.write({'properties': {'text': row[1], 'party': row[0]},'geometry': mapping(point)})
+
 
 #This collection of functions will be able to visualize our tweet datasets and compare them to election results
 def populate_index(idx, name_file_in):
     count = 0
     with fiona.open(name_file_in, 'r') as shp_input:
         for point in shp_input:
-            idx.insert(count, Polygon(point['geometry']['coordinates'][0].bounds))
+            idx.insert(count, Point(point['geometry']['coordinates'][0].bounds))
             count = count + 1
 
 #This function cleans the fiona shapefile and creates the count file                                   
@@ -40,10 +41,10 @@ def finalize_tweets(state_set, red_set, blue_set, blue_idx, red_idx):
     with fiona.open(state_set, 'r') as states:
         newschema = states.schema.copy()
         newschema['properties']['count'] = 'float'
-        with fiona.open(red_set, 'w', crs = , driver = 'ESRI Shapefile', schema = newschema) as red:
+        with fiona.open(driver="ESRI Shapefile", schema = newschema, crs = from_epsg(4296)) as red:
             for state in red:
                 state['properties']['count'] = idx.count(Polygon(state['geometry']['coordinates'][0].bounds))
-            with fiona.open(blue_set, 'w', crs = , driver = 'ESRI Shapefile', schema = newschema) as blue:
+            with fiona.open(driver="ESRI Shapefile", schema = newschema, crs = from_epsg(4296)) as blue:
                 for state in blue:
                     state['properties']['count'] = idx.count(Polygon(state['geometry']['coordinates'][0].bounds))
                  
